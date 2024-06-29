3a8b649dd056960b77a5423e4cbb8603728b94f7
viveksingh2310 <viveksingh14538@gmail.com>
2024-06-29 22:58:59 +0530

    Add docs
    Added the Users docs only to the API by swagger-ui for better readability and understanding of API working .

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser';
+import swagger from 'swagger-ui-express'
 import cors from 'cors';
 import { ProductRouter } from './src/features/product/product.routes.js';
 import { UserRouter } from './src/features/users/users.routes.js';
@@ -7,14 +8,17 @@ import { RateRouter } from './src/features/rate/rate.routes.js';
 import { jwtAuth } from './src/middlewares/user.authentication.js';
 import CartRouter from './src/features/cart/cart.routes.js';
 import loggerMiddleware from './src/middlewares/logger.middleware.js';
+import apiDocs from './swagger.json' assert {type:"json"}
 export const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 var corsOptions={
-    origin:'http://127.0.0.1:3000'}
+     origin:'http://127.0.0.1:3000'
+    }
 
 app.use(cors(corsOptions));
 app.use(loggerMiddleware);
+app.use('/docs',swagger.serve,swagger.setup(apiDocs));
 app.get('/', (req, res) => {
     return res.status(200).send('you are in the index page of the API');
 });
