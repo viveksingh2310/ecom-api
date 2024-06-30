00c2b61ede3051822acaf75bc5870c3beea41a20
viveksingh2310 <viveksingh14538@gmail.com>
2024-06-30 20:14:37 +0530

    Done swagger.json
    Completed the whole documentation of the E-COM API using swagger-ui-express.

--- src/features/cart/cart.routes.js
+++ src/features/cart/cart.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import CartController from "./cart.controller.js";
  const CartRouter=new Router();
- CartRouter.get('/',CartController.get);
- CartRouter.post('/items',CartController.getProducts);
- CartRouter.put('/add',CartController.add);
- CartRouter.delete('/del',CartController.deleteProduct);
- CartRouter.post('/update',CartController.updateCart);
+ CartRouter.get('/',CartController.get);//
+ CartRouter.post('/items',CartController.getProducts);//
+ CartRouter.put('/add',CartController.add);//
+ CartRouter.delete('/del',CartController.deleteProduct);//
+ CartRouter.post('/update',CartController.updateCart);//
  export default CartRouter;
